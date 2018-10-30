c381240616463977e62156807e11354ed05db43d
LiJuons <kakarie2@gmail.com>
2018-10-30 17:40:00 +0200

    Initial push

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, NavigatorIOS} from 'react-native';
+
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Github Notetaker',
+          component: Main
+        }}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111111',
+  },
+});
---
+++ App/Components/Main.js
@@ -0,0 +1,130 @@
+import React, { Component } from 'react';
+import {StyleSheet, Text, View, TextInput, TouchableHighlight, ActivityIndicatorIOS} from 'react-native';
+import api from '../Utils/api';
+
+class Main extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '',
+      isLoading: false,
+      error: false
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      username: event.nativeEvent.text
+    });
+  }
+
+  handleSubmit = () => {
+    //update our indicatorIOS spinner
+    this.setState({
+      isLoading: true
+    });
+    //fetch data from Github
+    api.getBio(this.state.username)
+      .then((res) => {
+        if (res.message === 'Not Found'){
+          this.setState({
+            error: 'User not found',
+            isLoading: false
+          });
+        } else {
+          //reroute to the next passing that github info
+          this.props.navigator.push({
+            title: res.name || 'Select an Option',
+            component: Dashboard,
+            passProps: {userInfo: res}
+          });
+          this.setState({
+            isLoading: false,
+            error: false,
+            username: ''
+          });
+        }
+      })
+  }
+
+  render() {
+    return (
+      <View style={styles.mainContainer}>
+
+        <Text style={styles.title}>Search for a Github user</Text>
+
+        <TextInput
+          style={styles.searchInput}
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.handleSubmit}
+          underlayColor="white"
+        >
+          <Text style={styles.buttonText}>SEARCH</Text>
+        </TouchableHighlight>
+
+        <ActivityIndicatorIOS
+          animating={this.state.isLoading}
+          color="#111"
+          size="large"
+        />
+
+        {
+          this.state.error &&
+          <Text>{this.state.error}</Text>
+        }
+
+      </View>
+    )
+  }
+}
+
+var styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        padding: 30,
+        marginTop: 65,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        backgroundColor: '#48BBEC'
+    },
+    title: {
+        marginBottom: 20,
+        fontSize: 25,
+        textAlign: 'center',
+        color: '#fff'
+    },
+    searchInput: {
+        height: 50,
+        padding: 4,
+        marginRight: 5,
+        fontSize: 23,
+        borderWidth: 1,
+        borderColor: 'white',
+        borderRadius: 8,
+        color: 'white'
+    },
+    buttonText: {
+        fontSize: 18,
+        color: '#111',
+        alignSelf: 'center'
+    },
+    button: {
+        height: 45,
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        borderColor: 'white',
+        borderWidth: 1,
+        borderRadius: 8,
+        marginBottom: 10,
+        marginTop: 10,
+        alignSelf: 'stretch',
+        justifyContent: 'center'
+    },
+});
+
+export default Main;
